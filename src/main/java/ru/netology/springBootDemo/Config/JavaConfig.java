9a3d54bffdb73f70a7f03b6e5298cc20c1853f1e
Boffka92 <boffka92@ya.ru>
2024-03-03 14:12:43 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/springBootDemo/Config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.springBootDemo.Config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springBootDemo.DevProfile;
+import ru.netology.springBootDemo.ProductionProfile;
+import ru.netology.springBootDemo.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
